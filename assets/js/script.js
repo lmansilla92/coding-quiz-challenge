19ef7edf42e0d2f401eb18e2637540789b0fcd61
Luis Mansilla <l_mansilla92@yahoo.com>
2023-08-26 00:22:41 -0500

    Added JavaScript to grab html elements that start the timer countdown when its triggered by a click on the Start Quiz button.

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Selecting timer element by class
+var timerEl = document.querySelector(".timer");
+var secondsLeft = 75;
+var startButton = document.querySelector(".start-quiz");
+
+// Declares function that sets timer
+function setTimer() {
+    // Declares variable for timer interval with a value of a setInterval function that makes the time countdown and displays the current time with textContent
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "Time: " + secondsLeft;
+
+        // If statement checks if timer is at 0 to run the function that stops the timer at 0;
+        if(secondsLeft === 0) {
+            // Stops the timer at 0 when if conditional is true
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+// Adds event listener to wait for the button to be clicked to call the setTimer function to begin countdown
+startButton.addEventListener('click', function () {
+    setTimer();
+});
+
