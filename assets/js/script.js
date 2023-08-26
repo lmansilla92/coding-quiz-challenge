34ed3edcdd37705323a15c7f790fdc6673dea401
Luis Mansilla <l_mansilla92@yahoo.com>
2023-08-26 07:37:17 -0500

    adding click event to start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Selecting timer element by class
 var timerEl = document.querySelector(".timer");
 var secondsLeft = 75;
+var startContainer = document.getElementById("#start-quiz-container");
 var startButton = document.querySelector(".start-quiz");
+
 
 // Declares function that sets timer
 function setTimer() {
@@ -18,8 +20,17 @@ function setTimer() {
     }, 1000);
 }
 
+// declares function that starts the quiz
+function startQuiz() {
+    startContainer.setAttribute("style", "display: none;");
+    }  
+
+
+
+// function that hides the main page
+
+
 // Adds event listener to wait for the button to be clicked to call the setTimer function to begin countdown
-startButton.addEventListener('click', function () {
-    setTimer();
-});
+startButton.addEventListener("click", startQuiz());
+
 
